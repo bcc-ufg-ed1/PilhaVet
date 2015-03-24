23083f76a8ac94a8262e94e1696ed23d3d98d2e5
Paulo Júnior <pauloafpjunior@hotmail.com>
2015-03-24 08:27:46 -0300

    malloc e include stdio
    Fiz uma alteração na chamada da função malloc e inclui a biblioteca
    stdio.h no arquivo UsaPilha.c

--- PilhaVet.c
+++ PilhaVet.c
@@ -3,9 +3,8 @@
 #include "PilhaVet.h"
 
 PilhaVet* criarPilha() {
-    PilhaVet* pilha = (PilhaVet*) malloc(sizeof(PilhaVet));
-    if (pilha == NULL) 
-        return NULL;
+    PilhaVet* pilha = malloc(sizeof(*pilha));
+    if (pilha == NULL) return NULL;
     pilha->topo = 0;
     return pilha;
 }
@@ -15,21 +14,20 @@ int liberarPilha(PilhaVet* pilha) {
         return ESTRUTURA_NAO_INICIALIZADA;
     free(pilha);
     pilha = NULL;
+    return OK;
 }
 
 int estahCheia(PilhaVet* pilha) {
     if (pilha == NULL)
         return ESTRUTURA_NAO_INICIALIZADA;
-    if (pilha->topo == TAM_MAX) 
-        return TRUE;
+    if (pilha->topo == TAM_MAX) return TRUE;
     return FALSE;
 }
 
 int estahVazia(PilhaVet* pilha) {
     if (pilha == NULL)
         return ESTRUTURA_NAO_INICIALIZADA;
-    if (pilha->topo == 0) 
-        return TRUE;
+    if (pilha->topo == 0) return TRUE;
     return FALSE;
 }
 
@@ -38,6 +36,7 @@ int empilhar(int item, PilhaVet* pilha) {
         return ESTRUTURA_NAO_INICIALIZADA;
     if (estahCheia(pilha))
         return ESTRUTURA_CHEIA;
+
     pilha->itens[pilha->topo] = item;
     pilha->topo++;
     return OK;
--- UsaPilha.c
+++ UsaPilha.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "PilhaVet.h"
 
 void imprimirPilha(PilhaVet* p) {
